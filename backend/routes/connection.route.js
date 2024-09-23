b002085daaad4b21c7fda7d29cefb89d8406ba89
yunyoungsik <yunyoungsik91@gmail.com>
2024-09-23 23:24:58 +0900

    [20241124]2:14:43 시작

---
+++ backend/routes/connection.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/request/:userId", protectRoute, sendConnectionRequest);
+router.put("/accept/:requestId", protectRoute, acceptConnectionRequest);
+router.put("/reject/:requestId", protectRoute, rejectConnectionRequest);
+// 현재 사용자에 대한 모든 연결 요청 가져오기
+router.get("/requests", protectRoute, getConnectionRequests);
+// 사용자의 모든 연결 가져오기
+router.get("/", protectRoute, getUserConnections);
+router.delete("/:userId", protectRoute, removeConnection);
+router.get("/status/:userId", protectRoute, getConnectionStatus);
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,21 @@ import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import notificationRoutes from './routes/notification.route.js';
+import connectionRoutes from './routes/connection.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // JSON 요청 본문을 파싱
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
+app.use('/api/v1/connections', connectionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
