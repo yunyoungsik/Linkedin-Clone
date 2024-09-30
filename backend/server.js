16aa90e490fc893695c0e8f12292553e5deaa312
yunyoungsik <yunyoungsik91@gmail.com>
2024-10-01 00:17:13 +0900

    [20241001]완성

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 // router
 import authRoutes from './routes/auth.route.js';
@@ -15,12 +16,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  // methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+      // methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+}
+
 app.use(express.json()); // JSON 요청 본문을 파싱
 app.use(cookieParser());
 
@@ -29,6 +36,14 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
 app.use('/api/v1/connections', connectionRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(_dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.mode === 'development' ? 'http://localhost:5000/api/v1' : '/api/v1',
   withCredentials: true,
-})
+});
